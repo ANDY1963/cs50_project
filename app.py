e279057a6805bc5c318b7fea09a369ac5d006d4f
ANDY1963 <dr.andrew.david.brown@gmail.com>
2020-12-05 20:14:37 +0000

    working app

--- app.py
+++ app.py
@@ -8,11 +8,10 @@ def peak_sun():
     """Generate chart and table"""
      # if GET just show input form 
     if request.method == "GET":
-        return render_template("index.html")
+        return render_template("index.html", title = "home")
 
     # if POST get user input from form
     if request.method == "POST":
-        print("doooooo")
         # get latitude from form
         lat=request.form.get("latitude")
         if not request.form.get("latitude"):
@@ -22,7 +21,9 @@ def peak_sun():
         lon=request.form.get("longitude")
         if not request.form.get("longitude"):
             return "must provide longitude"
-        else:
-            return "hello"
+        # Call helper function from helpers.py; return value is a tuple of two lists
+        data = contact_API(lat, lon)
+        
+        return render_template("chart.html", title = "chart", irrad_data = data[0], month_data = data[1])
 
         
--- helpers.py
+++ helpers.py
@@ -9,22 +9,22 @@ def contact_API(lat, lon):
     # Contact API. Python requests library method returns a response object. 
     response = requests.get(f"https://re.jrc.ec.europa.eu/api/MRcalc?startyear=2016&endyear=2016&lat={lat}&lon={lon}&outputformat=json&browser=1&horirrad=1")
     # json method gets data from the object
-    return response.json()
+    data = response.json()
 
-lat = 51
-lon = -3
-data = contact_API(lat, lon)
-# Extract list of output dicts for each month
-output_list = data["outputs"]["monthly"]
-# Initialise python dicts for irradiance and month data
-irrad_data = []
-month_data = []
-# Loop output_list to extract 
-for item in output_list:
-    irrad_data.append(item["H(h)_m"])
-    month_data.append(item["month"])
-#print(irrad_data)
-#print(month_data)
+    # Extract list of output dicts for each month
+    output_list = data["outputs"]["monthly"]
+
+    # Initialise python dicts for irradiance and month data
+    irrad_data = []
+    month_data = []
+
+    # Loop output_list to extract 
+    for item in output_list:
+        irrad_data.append(item["H(h)_m"])
+        month_data.append(item["month"])
+        
+    # Return tuple containing two lists
+    return (irrad_data, month_data)
 
 
 
